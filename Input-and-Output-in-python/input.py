e36a1d86a924f2b90ee1c3f3d8c2a4f8c8dbcbcd
josephsamimmanuel <josesamimmanuel@gmail.com>
2025-04-07 16:33:43 +0530

    Add input.py to demonstrate the usage of the input() function in Python, including examples of prompting for user input and type conversion.

---
+++ Input-and-Output-in-python/input.py
@@ -0,0 +1,43 @@
+# Input in Python
+
+# input() function
+
+# input() function with prompt
+
+name = input("Enter your name: ")
+print("Hello,", name)                # Hello, John
+
+# input() function with type conversion
+
+age = int(input("Enter your age: "))
+print("You are", age, "years old")   # You are 20 years old
+
+# input() function with type conversion
+
+num = float(input("Enter a number: "))
+print("You entered:", num)           # You entered: 12.5
+
+# input() function with type conversion
+
+num = int(input("Enter a number: "))
+print("You entered:", num)           # You entered: 12
+
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+print("Name:", name)
+print("Age:", age)               # Name: John
+
+x = int(input("Enter a number: "))
+y = int(input("Enter another number: "))
+
+print("Sum:", x + y)               # Sum: 30
+
+
+
+
+
+
+
+
+
