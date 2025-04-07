81e6c24edb94fdfe9de8d5e86749960a75a51317
josephsamimmanuel <josesamimmanuel@gmail.com>
2025-04-07 10:10:12 +0530

    variables in python

---
+++ variables.py
@@ -0,0 +1,56 @@
+# Variables in python
+
+age = 32
+name = "JOSEPH"
+weight = 65
+
+print(age)
+print(name)
+print(weight)
+
+# -----------------------------------------------------------
+
+price = 10
+tax = 18
+total = price + tax
+
+print(total)
+
+# -----------------------------------------------------------
+
+x = 10
+y = "Hello"
+z = 10.5
+w = z
+w = 30
+
+print(x)
+print(y)
+print(z)
+print(w)
+print(x, y, z, w)
+
+# -----------------------------------------------------------
+
+# Variables in python are dynamic
+
+x = 10
+x = "Hello"
+
+print(x) 
+
+# -----------------------------------------------------------
+
+is_valid = True
+marks = 85
+pi = 3.14
+city_name = "New York"
+
+print(is_valid)
+print(marks)
+print(pi)
+print(city_name)
+
+# -----------------------------------------------------------
+a = None
+print(a)
