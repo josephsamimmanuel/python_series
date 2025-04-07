e9a78c9e5065c8ad64d3f97e9e701c496d3f8e35
josephsamimmanuel <josesamimmanuel@gmail.com>
2025-04-07 16:24:20 +0530

    Add print.py to demonstrate the usage of the print() function in Python, including examples of multiple arguments, end, and sep parameters.

---
+++ Input-and-Output-in-python/print.py
@@ -0,0 +1,42 @@
+# Print in Python
+
+# print() function
+
+# print() function with multiple arguments
+
+# end parameter
+print("Hello", "World", "!")          # Hello World !
+
+# sep parameter
+print("Welcome", "to", "GeeksforGeeks")          # Welcome to GeeksforGeeks
+
+# end parameter
+print()                                      # 
+
+# end parameter
+print("Hope you are enjoying", end=" ")          # Hope you are enjoying GeeksforGeeks
+print("GeeksforGeeks")
+
+# end parameter
+print("GeeksforGeeks", end=" ")          # GeeksforGeeks GeeksforGeeks
+print("GeeksforGeeks") 
+
+# sep parameter
+print("25", "08", "2025", sep="-")          # 25-08-2025
+
+# sep parameter
+print("10","20","30", sep="+", end=" ")          # 10+20+30 40 50
+print(40,50)
+
+
+
+
+
+
+
+
+
+
+
+
+
