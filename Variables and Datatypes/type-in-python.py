d7f23dd097f8c734f30824a54e317663fef5f5ce
josephsamimmanuel <josesamimmanuel@gmail.com>
2025-04-07 10:53:30 +0530

    Add type-in-python.py to illustrate various data types in Python and their usage

---
+++ Variables and Datatypes/type-in-python.py
@@ -0,0 +1,49 @@
+# Type in python
+
+a = 10                                              # int ✔️
+b = 10.5                                            # float ✔️
+c = "Hello"                                         # str ✔️
+d = True                                            # bool ✔️
+e = None                                            # NoneType ✔️
+f = [1, 2, 3]                                       # list ✔️
+g = (1, 2, 3)                                       # tuple ✔️
+h = {"name": "John", "age": 20}                     # dict ✔️
+i = {"apple", "banana", "cherry"}                   # set ✔️
+j = range(10)                                       # range
+k = {"key1": "value1", "key2": "value2"}            # dict ✔️
+l = b"Hello"                                        # bytes
+m = bytearray(5)                                    # bytearray
+n = memoryview(bytes(5))                            # memoryview
+o = complex(1, 2)                                   # complex ✔️
+p = set(("apple", "banana", "cherry"))              # set ✔️
+q = frozenset(("apple", "banana", "cherry"))        # frozenset ✔️
+r = dict(name="John", age=20)                       # dict ✔️
+s = tuple(("apple", "banana", "cherry"))            # tuple ✔️
+t = list(("apple", "banana", "cherry"))             # list ✔️
+v = 2+3j                                            # complex ✔️
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+print(type(g))
+print(type(h))
+print(type(i))
+print(type(j))
+print(type(k))
+print(type(l))
+print(type(m))
+print(type(n))
+print(type(o))
+print(type(p))
+print(type(q))
+print(type(r))
+print(type(s))
+print(type(t))
+print(type(v))
+# ----------------------------------------------------------------  
+
+
+
