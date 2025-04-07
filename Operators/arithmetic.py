85f36c84bd20d60e03a444ec42cde86d5916f31a
josephsamimmanuel <josesamimmanuel@gmail.com>
2025-04-07 16:52:01 +0530

    Add arithmetic.py to demonstrate arithmetic operators in Python, including examples of addition, subtraction, multiplication, division, modulus, exponentiation, and floor division, as well as operator precedence and associativity.

---
+++ Operators/arithmetic.py
@@ -0,0 +1,116 @@
+# Arithmetic Operators in Python
+
+# Addition
+# Subtraction
+# Multiplication
+# Division
+# Modulus
+# Exponentiation
+# Floor Division
+
+# Addition
+a = 10
+b = 20
+print(a + b)          # 30
+
+# Subtraction
+a = 10
+b = 20
+print(a - b)          # -10
+
+# Multiplication
+a = 10
+b = 20
+print(a * b)          # 200
+
+# Division
+a = 10
+b = 20
+print(a / b)          # 0.5
+
+# Modulus
+a = 10
+b = 20
+print(a % b)          # 10  
+
+# Exponentiation
+a = 10
+b = 20
+print(a ** b)          # 100000000000000000000
+
+# Floor Division
+a = 100
+b = 20
+print(a // b)          # 5
+
+# ------------------------------------------------------------
+
+x = -5
+y = 2
+
+print(x // y)          # -3
+
+x = 5.0
+y = 2
+
+print(x // y)          # 2.0
+
+x = 2
+y = 2
+
+print(x ** y)          # 4
+
+# ------------------------------------------------------------
+
+# Precedence of Arithmetic Operators
+
+# + and - have the same precedence
+# * and / have the same precedence
+# ** has the highest precedence
+
+# Precedence of Arithmetic Operators
+
+print(5+2*3)          # 11
+
+print(5+2**3)          # 13
+
+print(5+2*3**2)          # 23
+
+print(5+2*3**2)          # 23
+
+# ------------------------------------------------------------
+
+# Associativity of Arithmetic Operators
+
+# + and - have left associativity
+# * and / have left associativity
+# ** has right associativity
+
+print(5+2-3)          # 4
+print(2**2**-1)          # 2
+print((2**2)**-1)          # 0.25
+
+# ------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
