4d251ffd7112ebeee44d6ca4c54a5a02de615d8f
josephsamimmanuel <josesamimmanuel@gmail.com>
2025-04-08 10:07:44 +0530

    Add while-loop.py to demonstrate the use of while loops in Python, including examples for printing numbers, repeated messages, and calculating sums of natural numbers, with user input for dynamic execution.

---
+++ Loops/while-loop.py
@@ -0,0 +1,48 @@
+# while loop is used to execute a block of code repeatedly until a condition is met.
+
+# syntax:
+# while condition:
+#     body of the loop
+
+# ----------------------------------------------------------------
+
+# example:
+
+i = 1
+while i <= 10:
+    print(i)
+    i += 1
+
+# ----------------------------------------------------------------
+
+# An example program to print "Hello World" 5 times
+
+i = 1
+while i <= 5:
+    print("Hello World")
+    i += 1
+
+# ----------------------------------------------------------------
+
+# An example program to print the sum of first 10 natural numbers
+
+i = 1
+sum = 0
+while i <= 10:
+    sum += i # sum = sum + i
+    i += 1
+
+print(sum)
+
+# ----------------------------------------------------------------
+
+n= int(input("Enter a number: "))
+i = 1
+while i<n+1:
+    print(f"Hello World {i}")
+    i += 1
+
+# ----------------------------------------------------------------
+
+# An example program to print the sum of first n natural numbers
+
