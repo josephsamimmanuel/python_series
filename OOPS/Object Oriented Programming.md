19140ea345d0e06b50417d4b09254fef40a27665
josephsamimmanuel <josesamimmanuel@gmail.com>
2025-04-10 13:11:08 +0530

    Add new Python files for Object Oriented Programming, including a class definition for a Person with methods to print name and age, and a markdown file introducing OOP concepts. These additions enhance understanding of OOP principles and class structures in Python.

---
+++ OOPS/Object Oriented Programming.md
@@ -0,0 +1,11 @@
+Introduction to Object Oriented Programming
+===========================================
+
+Procedural Programming:
+=======================
+    We break the code into a set of functions and these functions call each other
+
+Object Oriented Programming:
+=============================
+    We break the code into a set of entities and these entities talk to each other,
+These entities have data ad methods
---
+++ OOPS/classes-objects.py
@@ -0,0 +1,25 @@
+# Object Oriented Programming
+
+# Class
+# 1. Define the class
+# 2. Create the object
+# 3. Call the methods
+
+# Object
+# 1. Create the object
+# 2. Call the methods
+
+# ------------------------------------------------------------
+
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    def print_name(self):
+        print(self.name)
+    def print_age(self):
+        print(self.age)
+
+
+
+
